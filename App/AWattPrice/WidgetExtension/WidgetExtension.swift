d70cbfc3e030ec9e8d0541e08947d4ebeb551a77
Léon Becker <lb@space8.me>
2022-07-24 22:15:11 +0200

    feat: Implemented  dynamic graph generation.

--- App/AWattPrice/WidgetExtension/WidgetExtension.swift
+++ App/AWattPrice/WidgetExtension/WidgetExtension.swift
@@ -75,6 +75,23 @@ import SwiftUI
 //    }
 //}
 
+// MARK: Extensions
+struct RoundedCorner: Shape {
+    var radius: CGFloat
+    var corners: UIRectCorner
+    
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape(RoundedCorner(radius: radius, corners: corners))
+    }
+}
+
 // MARK: Prices Widget
 
 struct PricesWidget_TimelineEntry: TimelineEntry {
@@ -109,32 +126,152 @@ struct PricesWidget_TimelineProvider: TimelineProvider {
 }
 
 struct PricesWidgetView_BarView: View {
+    let isNegative: Bool
+    
+    init(isNegative: Bool = false) {
+        self.isNegative = isNegative
+    }
+    
+    var positiveColor = Color(red: 0.86, green: 0.65, blue: 0.24, opacity: 1.0)
+    var negativeColor = Color(red: 0.15, green: 0.86, blue: 0.06, opacity: 1.0)
+    
+    var cornerRadiusCorners: UIRectCorner {
+        if isNegative {
+            return [.bottomLeft, .bottomRight]
+        } else {
+            return [.topLeft, .topRight]
+        }
+    }
+    
     var body: some View {
         Rectangle()
-            .fill(Color(red: 0.86, green: 0.65, blue: 0.24, opacity: 1.0))
+            .fill(isNegative ? negativeColor : positiveColor)
+            .cornerRadius(3, corners: cornerRadiusCorners)
     }
 }
 
 struct PricesWidgetView_ChartView: View {
-    let energyPrices: [EnergyPricePoint]
+    struct Graph {
+        enum ValueRange {
+            case positive, negative, positiveAndNegative
+        }
+        
+        let energyPrices: [EnergyPricePoint]
+        let geometry: GeometryProxy
+        
+        var maxPrice: Double { (energyPrices.max { $1.marketprice > $0.marketprice ? true : false })?.marketprice ?? 0 }
+        var minPrice: Double { (energyPrices.min { $1.marketprice > $0.marketprice ? true : false })?.marketprice ?? 0 }
+        var valueScope: ValueRange {
+            if minPrice >= 0 { return .positive }
+            else if maxPrice <= 0 { return .negative }
+            else { return .positiveAndNegative }
+        }
+        
+        /// The factor by which the partition step is divisable.
+        var partitionFactor: Double = 5
+        var partitionAmount: Double = 3
+        /// Each partition's value is a multiple of this value. The step is divisable by the partition factor.
+        var partitionStep: Double {
+            var graphPriceRange: Double
+            if valueScope == .positive {
+                graphPriceRange = self.maxPrice
+            } else if valueScope == .negative {
+                graphPriceRange = self.minPrice.magnitude
+            } else {
+                let graphMinPrice = getNextPriceDivisibleByPartitionFactor(price: minPrice)
+                graphPriceRange = graphMinPrice.magnitude+self.maxPrice
+            }
+            
+            var step = graphPriceRange / self.partitionAmount
+            step = getNextPriceDivisibleByPartitionFactor(price: step)
+            return step
+        }
+        
+        var priceRange: Double {
+            self.partitionStep * self.partitionAmount
+        }
+        var minGraphPrice: Double {
+            if valueScope == .positive { return 0 }
+            else if valueScope == .negative { return priceRange }
+            else { return getNextPriceDivisibleByPartitionFactor(price: minPrice) }
+        }
+
+        var positiveStartHeight: CGFloat {
+            return geometry.size.height-calculateBarHeight(price: minGraphPrice).magnitude
+        }
+        
+        var barSpacing: CGFloat = 1.8
+        var barWidth: CGFloat {
+            let widthAvailable = geometry.size.width - self.barSpacing * CGFloat(energyPrices.count-1)
+            return widthAvailable / CGFloat(energyPrices.count)
+        }
+        
+        var lines: [Double] {
+            var lines: [Double] = [0]
+            for x in stride(from: 0, to: partitionAmount+1, by: 1) {
+                lines.append(minGraphPrice+x*partitionStep)
+            }
+            return lines
+        }
+        
+        func getNextPriceDivisibleByPartitionFactor(price: Double) -> Double {
+            var nextPrice = price.magnitude
+            let nextPriceRemainder = nextPrice.magnitude.truncatingRemainder(dividingBy: self.partitionFactor)
+            if nextPriceRemainder != 0 {
+                nextPrice = (nextPrice-nextPriceRemainder)+self.partitionFactor
+            }
+            if price.sign == .minus {
+                nextPrice.negate()
+            }
+            return nextPrice
+        }
+        
+        func calculateBarHeight(price: Double) -> CGFloat {
+            return geometry.size.height * (price / self.priceRange)
+        }
+        
+        func getYCoodinate(forPrice price: Double) -> CGFloat {
+            let priceBarHeight = calculateBarHeight(price: price)
+            return positiveStartHeight - priceBarHeight
+        }
+    }
     
-    var highestPrice: Double {
-        energyPrices.max { $1.marketprice > $0.marketprice ? true : false }?.marketprice ?? 0
+    let graph: Graph
+    
+    init(energyPrices: [EnergyPricePoint], geometry: GeometryProxy) {
+        self.graph = Graph(energyPrices: energyPrices, geometry: geometry)
+    }
+    
+    var bars: some View {
+        HStack(alignment: .bottom, spacing: graph.barSpacing) {
+            ForEach(graph.energyPrices, id: \.startTime) { pricePoint in
+                let height = graph.calculateBarHeight(price: pricePoint.marketprice)
+                
+                PricesWidgetView_BarView(isNegative: pricePoint.marketprice.sign == .minus)
+                    .frame(width: graph.barWidth, height: height.magnitude)
+                    .position(x: graph.barWidth/2, y: -(height/2)+graph.getYCoodinate(forPrice: 0))
+            }
+        }
+    }
+    
+    var lines: some View {
+        ZStack {
+            ForEach(graph.lines.indices, id: \.self) { lineIndex in
+                let linePrice = graph.lines[lineIndex]
+
+                Rectangle()
+                    .frame(width: graph.geometry.size.width, height: 1)
+                    .foregroundColor(.black)
+//                    .opacity(0.5)
+                    .position(x: graph.geometry.size.width/2, y: graph.getYCoodinate(forPrice: linePrice))
+            }
+        }
     }
     
     var body: some View {
-        GeometryReader() { geometry in
-            let barSpacing: CGFloat = 1
-            
-            HStack(alignment: .bottom, spacing: barSpacing) {
-                ForEach(energyPrices, id: \.startTime) { pricePoint in
-                    let width = (geometry.size.width - barSpacing * CGFloat(energyPrices.count-1)) / CGFloat(energyPrices.count)
-                    let height = geometry.size.height * (CGFloat(pricePoint.marketprice) / CGFloat(highestPrice))
-
-                    PricesWidgetView_BarView()
-                        .frame(width: width, height: height, alignment: .bottom)
-                }
-            }
+        ZStack {
+            bars
+            lines
         }
     }
 }
@@ -155,7 +292,9 @@ struct PricesWidgetView: View {
             }
             
             if let next24hPrices = next24hPrices {
-                PricesWidgetView_ChartView(energyPrices: next24hPrices)
+                GeometryReader { geometry in
+                    PricesWidgetView_ChartView(energyPrices: next24hPrices, geometry: geometry)
+                }
             }
         }
         .padding([.top, .bottom, .leading, .trailing], 16)
